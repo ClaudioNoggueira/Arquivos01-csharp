1ac3790e99a9b49279931a5fbf58d2df1ed7cba5
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-27 23:19:41 -0300

    First commit

---
+++ Arquivos01-csharp/Entitites/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Entitites {
+    class Product {
+        public string Name { get; set; }
+        public double Preco { get; set; }
+        public int Quantity { get; set; }
+        
+        public Product() {
+
+        }
+
+        public Product(string name, double preco, int quantity) {
+            this.Name = name;
+            this.Preco = preco;
+            this.Quantity = quantity;
+        }
+
+        public double Total() {
+             return Preco * Quantity;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Total().ToString("F2");
+        }
+    }
+}
---
+++ Arquivos01-csharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Entitites;
+using System;
+using System.IO;
+
+namespace Arquivos01_csharp {
+    class Program {
+        private static string sourcePath;
+
+        static void Main(string[] args) {
+            Console.Write("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string line in lines) {
+                        string[] fields = line.Split(",");
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product);
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
